a937f42581a0f70f39e6d05ab40ab8b34a58fc14
Sapar Sayat <saparsayat01@gmail.com>
2024-05-25 16:42:33 +0500

    refactor

--- src/main/java/kz/runamicon/socialnetwork/config/OpenApiConfig.java
+++ src/main/java/kz/runamicon/socialnetwork/config/OpenApiConfig.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.Info;
                 title = "Runamicon Social Network Documentation",
                 description = "Social Network", version = "0.0.1",
                 contact = @Contact(
-                        name = "Kanishev Ilya",
+                        name = "Kanishev Ilya X",
                         email = "kanishevilya2@gmail.com"
                 )
         )
